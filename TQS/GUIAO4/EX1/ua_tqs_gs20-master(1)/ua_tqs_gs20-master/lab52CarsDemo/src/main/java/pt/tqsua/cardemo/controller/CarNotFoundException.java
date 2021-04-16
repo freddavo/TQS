b9baffcd0360a8388b1fa6d967327dbb4ab311f1
Your Name <freddavo>
2021-04-16 14:26:51 +0100

    guiao4-update_ex1

--- TQS/GUIAO4/EX1/ua_tqs_gs20-master(1)/ua_tqs_gs20-master/lab52CarsDemo/src/main/java/pt/tqsua/cardemo/controller/CarNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package pt.tqsua.cardemo.controller;
-
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
-@ResponseStatus(HttpStatus.NOT_FOUND)
-public class CarNotFoundException extends RuntimeException {
-
-}
